ea08a7f17c04c6b025787f64420ada2d1bb78ea0
Neto Falcone <netofalcone@gmail.com>
2018-12-28 20:52:28 -0200

    verificador full

--- script4.js
+++ script4.js
@@ -2,16 +2,34 @@
 /* comentário bloco*/
 
 function testeVerificador() {
-
 	var x = prompt("Digite um número: ");
 
 	if(x > 10) {
-
      alert("É maior que 10!!!!");
 
  	} else{
-
- 		alert("Não é maior que 10!");
+ 		alert("n�o � maior que 10!");
  	}
-}
+ }
+	
+//
 
+var x = 0;
+	document.write("Iniciando o loop while... <br/>");
+
+
+while(x < 10) {
+
+	document.write("numero:" + x +" <br/>");
+	x++;
+}
+	document.write("finalizando o loop while" + "<br/>" + "<br/>" + "<br/>");
+
+
+//
+
+	document.write("Iniciando for... <br/>");
+for(x = 0; x < 30; x++){
+	document.write("Numero:" + x + " <br/>");
+}
+	document.write("finalizando o for... <br/>");
