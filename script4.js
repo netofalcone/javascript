658ab59187f5efff4644a58a25cd7781bc302363
Neto Falcone <netofalcone@gmail.com>
2018-12-28 20:33:04 -0200

    verificador

---
+++ script4.js
@@ -0,0 +1,17 @@
+//comentário linha
+/* comentário bloco*/
+
+function testeVerificador() {
+
+	var x = prompt("Digite um número: ");
+
+	if(x > 10) {
+
+     alert("É maior que 10!!!!");
+
+ 	} else{
+
+ 		alert("Não é maior que 10!");
+ 	}
+}
+
